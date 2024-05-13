d82d8ca54a43eac499d6d2adb7015528af4974a5
Yanis <yvroland@misyl.net>
2024-05-13 12:41:22 +0200

    Cleancode

--- lib/app/theme/app_const.dart
+++ lib/app/theme/app_const.dart
@@ -3,8 +3,8 @@ class AppConst {
   static const String appName = 'MyTechLib';
   static const String appVersion = '1.0.0';
 
-  static const String baseUrl = 'http://192.168.1.177:3000';
-  // static const String baseUrl = 'http://16.171.1.190:3000';
+  // static const String baseUrl = 'http://192.168.1.177:3000';
+  static const String baseUrl = 'http://16.171.1.190:3000';
   static const String anonToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFyeHVwZm9laXl5dm93Y2JreXp3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDcyMjEwMDYsImV4cCI6MjAyMjc5NzAwNn0.lDcM6CgAk3dAKhtke5q66beviyptGvhLDNemvhhJlTE';
 
   // Endpoints
